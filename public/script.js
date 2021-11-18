0f67a67364791d2ab3933d9688bde07c0ebc7a37
lirasusejdev <lis.amaral@grupoboticario.com.br>
2021-11-18 10:00:39 -0300

    add webpack

---
+++ public/script.js
@@ -0,0 +1,5 @@
+/**
+ * Babel: conversion code refresh - conversao de codigo react para que o browser entenda. 
+ * Webpack: para cada tipo de arquivo - converter de maneira diferente
+ * Loaders: babel-loader, css-loader, 
+ */
